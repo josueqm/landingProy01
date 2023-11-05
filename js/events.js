27e6bcd60156d683a5eabcbab05da6751bd9c7eb
Joel26m <jmarquezu2223@politecnics.barcelona>
2023-11-05 15:47:07 +0100

    header js y css

--- js/events.js
+++ js/events.js
@@ -3,9 +3,8 @@ window.addEventListener('scroll', function() {
     var boton = document.getElementById('boton');
     var imagen = document.getElementById('imagen_logo');
     if (window.scrollY > 0) {
-        navbar.style.backgroundColor = '#1E3932'; /* Cambia el color de fondo al hacer scroll */
+        navbar.style.backgroundColor = 'rgba(30, 57, 50, 0.7)'; /* Cambia el color de fondo al hacer scroll */
         navbar.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)'; /* Agrega sombra */
-        boton.style.backgroundColor = '#F7F7F7';
         boton.style.Color = '#1E3932';
         navbar.classList.add('shrink');
         imagen
